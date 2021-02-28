ae4bdc522d136b05179d3c0048985bf300927d1b
csiziszebasztian <tanulas88@gmail.com>
2021-02-28 13:11:35 +0100

    layout CSS

--- burger-builder-app/src/components/layout/Layout.js
+++ burger-builder-app/src/components/layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import Auxi from '../../containers/hoc/Auxi';
+import Auxi from '../../hoc/Auxi';
+import classes from './Layout.css';
 
 const layout =(props)=> (
     <Auxi>
         <div>Toolbar, SideBar, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxi>
